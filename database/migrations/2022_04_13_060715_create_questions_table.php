aa7132f612b0e4982852c63bfaa44e752e0427b3
UladzislauZubets <uladzislau.zubets@innowise-group.com>
2022-04-15 10:01:38 +0300

    api

---
+++ database/migrations/2022_04_13_060715_create_questions_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('questions', function (Blueprint $table) {
+            $table->id();
+
+            $table->string('name', 128)->unique();
+            $table->string('type')->default(config('app.available_question_types')['Выбор одного']);
+
+            $table->unsignedInteger('order')->unique();
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('questions');
+    }
+};
